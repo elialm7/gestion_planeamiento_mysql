6d06b3c75a5f553e98e3c2f11a3b3f13ef9a8291
elialm7 <metall65eli@gmail.com>
2025-03-04 13:51:18 -0300

    feat: se aniaden mas atributos y creation de la table roles

--- sql_creation_tables.sql
+++ sql_creation_tables.sql
@@ -6,9 +6,18 @@ CREATE TABLE IF NOT EXISTS `tb_usuarios`(
 	`id_user` INT PRIMARY KEY AUTO_INCREMENT NOT NULL, 
     `nombre` VARCHAR(100) NOT NULL, 
     `correo` VARCHAR(100), 
-    `password` TEXT
-    
+    `password` TEXT, 
+    `descripcion` TEXT, 
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+    `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)ENGINE = INNODB;
+
+CREATE TABLE IF NOT EXISTS `tb_roles` (
+	`id_roles` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `NOMBRE` VARCHAR(50) UNIQUE NOT NULL, 
+    `id_user` INT NOT NULL,
+     CONSTRAINT `fk_user_role` FOREIGN KEY(`id_user`) REFERENCES `tb_usuarios`(`id_user`)
+) ENGINE = INNODB;
 
 
-)engine = innodb;
 
