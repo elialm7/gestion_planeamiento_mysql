6d589971090c063b312aed734da575a078827790
elialm7 <metall65eli@gmail.com>
2025-03-04 14:07:22 -0300

    improv: aniadicion de comentarios y mas atributos

--- sql_creation_tables.sql
+++ sql_creation_tables.sql
@@ -1,7 +1,9 @@
 CREATE  DATABASE IF NOT EXISTS `gestion_planeamiento`;
 
 USE `gestion_planeamiento`;
-
+/*
+  Creacion de la tabla usuarios
+*/
 CREATE TABLE IF NOT EXISTS `tb_usuarios`(
 	`id_user` INT PRIMARY KEY AUTO_INCREMENT NOT NULL, 
     `nombre` VARCHAR(100) NOT NULL, 
@@ -12,10 +14,15 @@ CREATE TABLE IF NOT EXISTS `tb_usuarios`(
     `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )ENGINE = INNODB;
 
+/*
+  Creacion de la tabla roles, 
+  La relacion entre roles y usuarios es de uno a uno
+  usuarios(1) -> roles(1)
+*/
 CREATE TABLE IF NOT EXISTS `tb_roles` (
 	`id_roles` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     `NOMBRE` VARCHAR(50) UNIQUE NOT NULL, 
-    `id_user` INT NOT NULL,
+    `id_user` INT UNIQUE NOT NULL,
      CONSTRAINT `fk_user_role` FOREIGN KEY(`id_user`) REFERENCES `tb_usuarios`(`id_user`)
 ) ENGINE = INNODB;
 
